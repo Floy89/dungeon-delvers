76d9f0c21fa6420d4df8295d40aae2174f3e7e05
Floy89 <Florian.Schlueter@outlook.com>
2026-05-24 22:40:01 +0200

    Initial commit

---
+++ utilities/import/import_walls.gd
@@ -0,0 +1,19 @@
+@tool
+extends EditorScenePostImport
+
+# Import Script for walls: Gives them collision meshes, and sets them to collision layer 2
+func _post_import(scene):
+	iterate(scene)
+	return scene
+
+func iterate(node):
+	if node != null:
+		if node is MeshInstance3D:
+			node.create_trimesh_collision()
+		if node is StaticBody3D:
+			node.set_collision_layer_value(1, false)
+			node.set_collision_layer_value(2, true) # Walls are on Collision Layer 2
+			node.set_collision_mask_value(1, false) # Walls don't need to detect anything, so don't need a mask.
+		for child in node.get_children():
+			iterate(child)
+			
